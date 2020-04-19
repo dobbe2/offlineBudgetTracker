1fa4793927ba44e139f27de71e36b3814a4ea0fa
James Dobbe <dobbe2@gmail.com>
2020-04-18 22:12:09 -0700

    finalized setting up DB, setting up to push to Heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,7 +8,8 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
     '/index.js',
     '/manifest.webmanifest',
-    '/styles.css'
+    '/styles.css',
+    '/db.js'
 ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -24,7 +25,7 @@ self.addEventListener('install', function(evt) {
     );
 
     self.skipWaiting();
-})
+});
 
 //activate cache
 self.addEventListener("activate", function(evt) {
@@ -44,18 +45,9 @@ self.addEventListener("activate", function(evt) {
     self.clients.claim();
 });
 
-//fetching the cache
-// self.addEventListener('fetch', function(evt){
-//     evt.respondWith(
-//         caches.open(DATA_CACHE_NAME).then(cache => {
-//             return cache.match(evt.request).then(response => {
-//                 return response || fetch(evt.request);
-//             });
-//         })
-//     );
-// });
-//fetching CACHE
-self.addEventListener('fetch', function(evt) {
+//fetch cache
+self.addEventListener('fetch', evt => {
+    //cache successful requests to the API
     if (evt.request.url.includes('/api/')){
         console.log('[Service Worker] Fetch (data)', evt.request.url);
         evt.respondWith(
@@ -66,7 +58,6 @@ self.addEventListener('fetch', function(evt) {
                     if (response.status === 200) {
                         cache.put(evt.request.url, response.clone());
                     }
-
                     return response;
                 })
                 .catch(err => {
